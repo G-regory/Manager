100e7d3a5531a49cba859239d0ab1ead0f3881ad
G-regory <38275622+G-regory@users.noreply.github.com>
2018-11-28 23:16:20 +0100

    Create DisplayInfo.java
    tout ça pour faire une méthode

---
+++ DisplayInfo.java
@@ -0,0 +1,56 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.nio.file.StandardOpenOption.APPEND;
+
+public class DisplayInfo {
+
+    Path managePath = Paths.get("manage.csv");
+
+
+
+
+
+    public void palmares() {
+        int nbInfoPalmares = 2;
+        List<String> titrePalmares = new ArrayList<>();
+        String []tabPalmares = new String [titrePalmares.size()];
+        List<String> reponsePalmares = new ArrayList<>();
+//        for(int i = 0;i < nbInfoPalmares;i++ ){
+        try {
+            Files.write(managePath, "Palmares,Champions League,Championships\n".getBytes(), APPEND);
+        } catch (IOException e) {
+            System.out.println("petit problème désolé pour écrire sur le fichier");
+        }
+
+        try{
+            List<String> list = Files.readAllLines(managePath);
+            titrePalmares=list;
+        }catch(IOException e){
+            System.out.println("Fichier indisponible");
+        }
+
+        tabPalmares = titrePalmares.get(1).split(",");
+        for(int i = 0 ; i < tabPalmares.length;i++){
+            if(tabPalmares[i].contains("Leag")){
+                System.out.println(tabPalmares[i]+": 11");
+            }else if(tabPalmares[i].contains("ships")){
+                System.out.println(tabPalmares[i]+": xx");
+            }else {
+                System.out.println(tabPalmares[i]);
+            }
+        }
+
+    }
+
+
+    }
+
+
+
+
+
