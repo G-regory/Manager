c0d6ed4bb7f7f202cf65211ec6e58566cdb17381
G-regory <38275622+G-regory@users.noreply.github.com>
2018-12-01 11:56:35 +0100

    Ajout méthode, suppression Paths & writes
    Ajout méthode
    palmares();
    displaySentence();

--- DisplayInfo.java
+++ DisplayInfo.java
@@ -1,56 +1,46 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.nio.file.StandardOpenOption.APPEND;
-
 public class DisplayInfo {
 
-    Path managePath = Paths.get("manage.csv");
+    Club real = new Club();
+
+    private List<String> questionManager = new ArrayList<>();
+    private List<String> reponseClub = new ArrayList<>();
+    
+    /**
+     * Affiche le palmares du club
+     */
+    public String palmares(int nbInfoPalmares, Club real) {
+        String displayPalmares,championsLeague="Champions League",championsShips="Championships" ;
+
+        questionManager.add(real.getNamePalmares());
+        questionManager.add(championsLeague);
+        questionManager.add(championsShips);
+        reponseClub.add("****************");
+        reponseClub.add(real.getChampionsLeague());
+        reponseClub.add(real.getChampionShips());
+
+       return displayPalmares=this.displaySentence(nbInfoPalmares,questionManager,reponseClub);
+    }
 
 
+    public String displaySentence(int nbInfo,List<String> askManager, List<String> answerClub ){
+        String displayPalmares = "";
 
-
-
-    public void palmares() {
-        int nbInfoPalmares = 2;
-        List<String> titrePalmares = new ArrayList<>();
-        String []tabPalmares = new String [titrePalmares.size()];
-        List<String> reponsePalmares = new ArrayList<>();
-        reponsePalmares.add(": 11");
-        reponsePalmares.add(": xx");
-//        for(int i = 0;i < nbInfoPalmares;i++ ){
-        try {
-            Files.write(managePath, "Palmares,Champions League,Championships\n".getBytes(), APPEND);
-        } catch (IOException e) {
-            System.out.println("petit problème désolé pour écrire sur le fichier");
-        }
-
-        try{
-            List<String> list = Files.readAllLines(managePath);
-            titrePalmares=list;
-        }catch(IOException e){
-            System.out.println("Fichier indisponible");
-        }
-
-        tabPalmares = titrePalmares.get(1).split(",");
-        for(int i = 0 ; i < tabPalmares.length;i++){
-            if(tabPalmares[i].contains("Leag")){
-                System.out.println(tabPalmares[i]+reponsePalmares.get(i-1));
-            }else if(tabPalmares[i].contains("ships")){
-                System.out.println(tabPalmares[i]+reponsePalmares.get(i-1));
-            }else {
-                System.out.println(tabPalmares[i]);
+        for (int i = 0; i <= nbInfo; i++) {
+            if (i == 0) {
+                displayPalmares += answerClub.get(i) + "\n";
+                displayPalmares += askManager.get(i) + "\n";
+                displayPalmares += answerClub.get(i) + "\n";
+            } else {
+                displayPalmares += askManager.get(i)+" :";
+                displayPalmares += answerClub.get(i) + "\n";
             }
         }
-
+        return displayPalmares;
     }
-
-
-    }
+}
 
 
 
