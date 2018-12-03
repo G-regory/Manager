c506558a5d6e3100b8d9290e40afa8d9494d7855
G-regory <38275622+G-regory@users.noreply.github.com>
2018-12-03 21:13:12 +0100

    création palmares

---
+++ Palmares.java
@@ -0,0 +1,24 @@
+package com.souleyman.carriel.Model;
+
+public class Palmares {
+
+
+    private int championsLeague;
+    private String championShips;
+
+    public int getChampionsLeague() {
+        return championsLeague;
+    }
+
+    public void setChampionsLeague(int championsLeague) {
+        this.championsLeague = championsLeague;
+    }
+
+    public String getChampionShips() {
+        return championShips;
+    }
+
+    public void setChampionShips(String championShips) {
+        this.championShips = championShips;
+    }
+}
