8f2162225d89c8ea4b205c563852eebd0adff7e7
G-regory <38275622+G-regory@users.noreply.github.com>
2018-12-03 20:45:43 +0100

    reset class

--- Manager.java
+++ Manager.java
@@ -1,31 +1,24 @@
-import java.util.ArrayList;
-import java.util.List;
+package com.souleyman.carriel.View;
+
+import com.souleyman.carriel.Model.Club;
 
 
 public class Manager {
-    static DisplayInfo info = new DisplayInfo();
+
 
     public static void main(String[] args) {
 
+        /***
+         * 1. Construire un club
+         ***/
 
-        /**
-         * Construire une equipe avec :
-         */
-        // 1 coach
-        // 3 goalKeepers
-        // 3 strikers (attaquants)
-        // 4 midfielders (milieux)
-
-        Manager greg = new Manager();
         Club real = new Club();
-        greg.showPalmares(real);
-        greg.showReal(real);
-        greg.showTeam(real);
 
 
-        /**
-         * Afficher les info du club
-         */
+        /***
+         * 2. Afficher les info du club suivant le format ci-après
+         ***/
+
         // *********************************************
         // Real Madrid
         // *********************************************
@@ -38,18 +31,20 @@ public class Manager {
         // Away Colors: Black, Black, Black
         // President: Florentino Perez
 
-        /**
-         * Afficher le palmares du club
-         */
         // *********************************************
         // Palmares
         // *********************************************
         // Champions League: 11
-        // Championships: xx*
+        // Championships: xx
 
-        /**
-         * Afficher l'effectif
-         */
+        /***
+         * 3. Afficher l'effectif du club
+         * Se limiter à :
+         * 1 coach
+         * 3 goalKeepers
+         * 3 strikers (attaquants)
+         * 4 midfielders (milieux)
+         ****/
         // *********************************************
         // Team
         // *********************************************
@@ -76,79 +71,7 @@ public class Manager {
         // 2. Karim Benzema
         // ...
 
-    }
-
-    public void showPalmares(Club pReal) {
-        List<String> answerListInfo = new ArrayList<>();
-        List<String> questionListInfo = new ArrayList<>();
-
-        String tabQuestion[] = {"Champions League","Championships"};
-
-        pReal.setNamePalmares("Palmares");
-        pReal.setChampionsLeague("11");
-        pReal.setChampionShips("xx");
-
-        String tabReponse[] ={pReal.getNamePalmares(),pReal.getChampionsLeague(),pReal.getChampionShips()};
-
-        this.incrementeTab(answerListInfo,questionListInfo,tabReponse,  tabQuestion);
-    }
-
-    public void showReal(Club pReal) {
-        List<String> listInfo = new ArrayList<>();
-        List<String> questionListInfo = new ArrayList<>();
-
-        String tabQuestion[] = {"Country", "City", "Creation", "Stadium", "Stadium capacity", "Home Colors", "Away Colors", "President"};
-
-        pReal.setNameReal("Real Madrid");
-        pReal.setCountry("Spain");
-        pReal.setCity("Madrid");
-        pReal.setCreation(1902);
-        pReal.setStade("Santiago Bernabeu");
-        pReal.setCapaciteStade(81044);
-        pReal.setCouleurHome("White, White, White");
-        pReal.setCouleurAway("Black, Black, Black");
-        pReal.setPresident("Florentino Perez");
-
-        String tabReponse[] = {pReal.getNameReal(), pReal.getCountry(), pReal.getCity(),String.valueOf(pReal.getCreation()),pReal.getStade(),String.valueOf(pReal.getCapaciteStade()),pReal.getCouleurHome(),pReal.getCouleurAway(),pReal.getPresident()};
-
-        this.incrementeTab(listInfo,questionListInfo,tabReponse,  tabQuestion);
-    }
-
-    public void showTeam(Club pReal) {
-        List<String> answerListInfo = new ArrayList<>();
-        List<String> questionListInfo = new ArrayList<>();
-
-        String tabQuestion[] = {"Season","Head Coach","Assistant Coach","Captain",};
-
-        pReal.setNameTeam("Team");
-        pReal.setSeason("2017-2018");
-        pReal.setCoach("Zinedine Zidane");
-        pReal.setAssistant("xxx");
-        pReal.setCaptain("Cristiano Ronaldo");
-
-
-        String tabReponse[] ={pReal.getNameTeam(),pReal.getSeason(),pReal.getCoach(),pReal.getAssistant(),pReal.getCaptain()};
-
-        this.incrementeTab(answerListInfo,questionListInfo,tabReponse,  tabQuestion);
-
-       
 
     }
 
-    public void incrementeTab(List<String>reponse, List<String>question, String [] tabAnswer,String []tabAsk  ){
-        for (int i = 0; i < tabAnswer.length; i++) {
-            reponse.add(tabAnswer[i]);
-        }
-
-        for (int i = 0; i < tabAsk.length; i++) {
-            question.add(tabAsk[i]);
-        }
-        System.out.println(info.recupInfo(reponse.size(), reponse, question));
-
-    }
-
-    public void constructionEquipe(){
-        String trait="---------------------------------------------";
-
-    }
 }
