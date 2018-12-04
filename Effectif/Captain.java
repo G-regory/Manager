85b31166fc6a6a026ec0e34e069ec1dd2002c845
G-regory <38275622+G-regory@users.noreply.github.com>
2018-12-04 09:27:57 +0100

    ajout to String

--- Effectif/Captain.java
+++ Effectif/Captain.java
@@ -3,6 +3,7 @@ package com.souleyman.carriel.Model.Effectif;
 public class Captain extends Joueur{
 
     private String position;
+    private boolean estCaptain;
 
 
     public String getPosition() {
@@ -11,5 +12,19 @@ public class Captain extends Joueur{
 
     public void setPosition(String position) {
         this.position = position;
+    }
+
+    public boolean isEstCaptain() {
+        return estCaptain;
+    }
+
+    public void setEstCaptain(boolean estCaptain) {
+        this.estCaptain = estCaptain;
+    }
+    @Override
+    public String
+    toString() {
+        return "Captain: " +
+                getName();
     }
 }
