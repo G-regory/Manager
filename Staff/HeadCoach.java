04f89a357b1d6bda156063fbc9c788a4c58e8578
G-regory <38275622+G-regory@users.noreply.github.com>
2018-12-04 09:28:58 +0100

    ajout to string

--- Staff/HeadCoach.java
+++ Staff/HeadCoach.java
@@ -10,4 +10,11 @@ public class HeadCoach {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String
+    toString() {
+        return "Head Coach: " +
+                getName();
+    }
 }
